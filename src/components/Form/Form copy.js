d4bc0667b6e49ca4f09526c239e159bbae35fec3
Ana Paula Cortes <anapaulacortesibarra@gmail.com>
2022-04-13 21:57:06 -0300

    form copy

--- src/components/Form/Form copy.js
+++ src/components/Form/Form copy.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import FormValidation from './FormValidation'
-import { getGenres, createVideogame } from '../../redux/actions'
+import { useState } from 'react'
 import style from './Form.module.css'
+import NavBar from '../Navbar/Navbar'
+import FormValidation from './FormValidation'
+import { createVideogame } from '../../redux/actions'
+import { useDispatch } from 'react-redux'
+
 
 
 const Form = () => {
@@ -11,44 +13,26 @@ const Form = () => {
     name: '',
     description: '',
     released: '',
-    rating: '',
-    genreId: [],
-    platforms: []
-
+    rating: ''
   }
 
-  const [input, setInput] = useState(initialValues);
-  const [error, setError] = useState();
-
   const dispatch = useDispatch()
-  let genres = useSelector((state) => state.genres)
 
-  useEffect(() => {
-    dispatch(getGenres())
-  }, [dispatch])
-
+  const [input, setInput] = useState(initialValues);
+  const [error, setError] = useState('')
 
   const handleInputChange = (e) => {
     setInput({
       ...input, [e.target.name]: e.target.value
     })
-    setError({
-      ...FormValidation({ ...input, [e.target.name]: e.target.value })
-    })
-  }
+    console.log(input)
+    setError(
+      FormValidation({ ...input, [e.target.name]: e.target.value }))
 
-  const handleGenreSelection = (e) => {
-    setInput({
-      ...input, genreId: [...input.genreId, e.target.value]
-    })
-    setError({
-      ...FormValidation({ ...input, genreId: [...input.genreId, e.target.value] })
-    })
   }
-
 
   const handleSubmit = (e) => {
-    if (input.name && input.description && input.released && input.rating && input.genreId && input.platforms) {
+    if (input.name && input.description && input.released && input.rating) {
       e.preventDefault();
       dispatch(createVideogame(input));
       alert("The game was succesfully Created!");
@@ -58,8 +42,7 @@ const Form = () => {
         description: '',
         released: '',
         rating: '',
-        genreId: [],
-        platforms: []
+
       });
 
     } else {
@@ -69,47 +52,49 @@ const Form = () => {
   }
 
 
+
   return (
-    <form onClick={(e) => handleSubmit(e)}>
-      <label> Name: </label>
-      <input name="name" value={input.name} onChange={(e) => handleInputChange(e)} />
-      {error.name && (<p className={style.error}>{error.name}</p>)}
-
-      <label> Description:: </label>
-      <input name="description" value={input.description} onChange={(e) => handleInputChange(e)} />
-      {error.description && (<p className={style.error}>{error.description}</p>)}
-
-      <label> Released: </label>
-      <input name="released" value={input.released} onChange={(e) => handleInputChange(e)} />
-      {error.released && (<p className={style.error}>{error.released}</p>)}
+    <div className={style.backgroundContainer}>
+      <NavBar />
+      <div className={style.container}>
+        <h3 className={style.titleText}>CREATE VIDEOGAME</h3>
+        <form >
+          <label className={style.text}> Name: </label>
+          <input className={style.input} name="name" value={input.name} onChange={(e) => handleInputChange(e)} />
+          {error.name && (<p className={style.error}>{error.name}</p>)}
 
 
-      <label> Rating: </label>
-      <input name="rating" value={input.rating} onChange={(e) => handleInputChange(e)} />
-      {error.rating && (<p className={style.error}>{error.rating}</p>)}
+          <label className={style.text}> Description: </label>
+          <input className={style.input} name="description" value={input.description} onChange={(e) => handleInputChange(e)} />
 
-      <label> Genres: </label>
-      <select name="genreId" value={input.genreId} multiple={true} onChange={(e) => handleGenreSelection(e)}>
-        <option value="" hidden>Select a genre</option>
-
-        {
-          Object.keys(genres).length
-            ? genres.map(genre =>
-              <option key={genre.id} name="genre" value={genre.id}>{genre.name}</option>
-            )
-            : 'Loading'
-        }
-      </select>
-
-      {error.genreId && (<p>{error.genreId}</p>)}
-
-      {/* <label> Platforms: </label>
-      <select >
+          <label className={style.text}> Released: </label>
+          <input className={style.input} name="released" value={input.released} onChange={(e) => handleInputChange(e)} />
+          {error.released && (<p className={style.error}>{error.released}</p>)}
 
 
-      </select> */}
-    </form>
+          <label className={style.text}> Rating: </label>
+          <input className={style.input} name="rating" value={input.rating} onChange={(e) => handleInputChange(e)} />
+          {error.rating && (<p className={style.error}>{error.rating}</p>)}
+
+
+
+
+
+          <button className={style.btn} onClick={(e) => handleSubmit(e)} >create</button>
+
+        </form >
+      </div>
+    </div>
   )
 }
 
 export default Form;
+
+
+// {
+//   Object.keys(genres).length
+//     ? genres.data.map(genre =>
+//       <option key={genre.id} name="genre" value={genre.id}>{genre.name}</option>
+//     )
+//     : 'Loading'
+// }
